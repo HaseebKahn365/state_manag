242549c3c6a922c9535c20afeaf9981a80b95cbc
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2024-06-01 21:58:40 +0500

    managing state at the indivisual widgets using the stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,13 @@ void main() {
   runApp(const App());
 }
 
+const urls = [
+  'https://th.bing.com/th/id/OIP.XzP-5z-2nZq0cqQFAxtFLgHaEo?w=235&h=180&c=7&r=0&o=5&pid=1.7',
+  'https://th.bing.com/th/id/OIP.XzP-5z-2nZq0cqQFAxtFLgHaEo?w=235&h=180&c=7&r=0&o=5&pid=1.7',
+  'https://th.bing.com/th/id/OIP.6p_Z1fMuoTD0nrKR5ex0hAHaFj?w=234&h=180&c=7&r=0&o=5&pid=1.7',
+  'https://th.bing.com/th/id/OIP.MPUscXSAaYFkP6xqB6sl4AHaE7?w=247&h=180&c=7&r=0&o=5&pid=1.7',
+];
+
 class App extends StatelessWidget {
   const App({super.key});
 
@@ -11,15 +18,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Gallery App',
-      home: Gallery(
-        title: 'Haseeb Gallery',
-        urls: [
-          'https://th.bing.com/th/id/OIP.XzP-5z-2nZq0cqQFAxtFLgHaEo?w=235&h=180&c=7&r=0&o=5&pid=1.7',
-          'https://th.bing.com/th/id/OIP.XzP-5z-2nZq0cqQFAxtFLgHaEo?w=235&h=180&c=7&r=0&o=5&pid=1.7',
-          'https://th.bing.com/th/id/OIP.6p_Z1fMuoTD0nrKR5ex0hAHaFj?w=234&h=180&c=7&r=0&o=5&pid=1.7',
-          'https://th.bing.com/th/id/OIP.MPUscXSAaYFkP6xqB6sl4AHaE7?w=247&h=180&c=7&r=0&o=5&pid=1.7',
-        ],
-      ),
+      home: Gallery(title: 'Haseeb Gallery', urls: urls),
     );
   }
 }
@@ -48,15 +47,36 @@ class Gallery extends StatelessWidget {
 
 //demo of injecting the state into the widget
 
-class Photo extends StatelessWidget {
+class Photo extends StatefulWidget {
   const Photo({super.key, required this.url});
   final url;
 
   @override
+  // ignore: no_logic_in_create_state
+  PhotoState createState() => PhotoState(url: url);
+}
+
+class PhotoState extends State<Photo> {
+  String url;
+  int index = 0;
+
+  PhotoState({required this.url});
+
+  onTap() {
+    setState(() {
+      index >= urls.length - 1 ? index = 0 : index++;
+    });
+    url = urls[index];
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(top: 10),
-      child: Image.network(url),
+      padding: const EdgeInsets.only(top: 10),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Image.network(url),
+      ),
     );
   }
 }
