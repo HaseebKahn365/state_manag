64b9a173945b3981433889d49567e98124d53c3f
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2024-06-01 19:23:38 +0500

    Injecting the state using declarative state

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,52 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Gallery App',
-      home: Photo(),
+      home: Gallery(
+        title: 'Haseeb Gallery',
+        urls: [
+          'https://th.bing.com/th/id/OIP.XzP-5z-2nZq0cqQFAxtFLgHaEo?w=235&h=180&c=7&r=0&o=5&pid=1.7',
+          'https://th.bing.com/th/id/OIP.XzP-5z-2nZq0cqQFAxtFLgHaEo?w=235&h=180&c=7&r=0&o=5&pid=1.7',
+          'https://th.bing.com/th/id/OIP.6p_Z1fMuoTD0nrKR5ex0hAHaFj?w=234&h=180&c=7&r=0&o=5&pid=1.7',
+          'https://th.bing.com/th/id/OIP.MPUscXSAaYFkP6xqB6sl4AHaE7?w=247&h=180&c=7&r=0&o=5&pid=1.7',
+        ],
+      ),
     );
   }
 }
 
-class Photo extends StatelessWidget {
-  const Photo({super.key});
-  final url = 'https://th.bing.com/th/id/R.10ddad628f46b4d79fccd1a049899ec1?rik=5hnZ6oFoGOn7eQ&riu=http%3a%2f%2fimages.unsplash.com%2fphoto-1566264956500-0549ed17e161%3fixlib%3drb-1.2.1%26q%3d80%26fm%3djpg%26crop%3dentropy%26cs%3dtinysrgb%26w%3d1080%26fit%3dmax%26ixid%3deyJhcHBfaWQiOjEyMDd9&ehk=Eq%2f3gtVGbPQAVHyWOxG%2b3tGILLT1DLgCih2a8FfZM74%3d&risl=&pid=ImgRaw&r=0';
+class Gallery extends StatelessWidget {
+  const Gallery({super.key, required this.title, required this.urls});
+  final List<String> urls;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(url);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: GridView.count(
+        primary: false,
+        crossAxisCount: 2,
+        children: List.of(
+          urls.map((e) => Photo(url: e)),
+        ),
+      ),
+    );
+  }
+}
+
+//demo of injecting the state into the widget
+
+class Photo extends StatelessWidget {
+  const Photo({super.key, required this.url});
+  final url;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 10),
+      child: Image.network(url),
+    );
   }
 }
